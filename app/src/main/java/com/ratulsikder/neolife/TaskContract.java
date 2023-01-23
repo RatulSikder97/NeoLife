53002255ee4aa78a5d1792654d799cbec871079c
ratul.sikder <ratulsikder104@gmail.com>
2023-01-23 17:18:59 +0600

    Change Task Schema

--- app/src/main/java/com/ratulsikder/neolife/TaskContract.java
+++ app/src/main/java/com/ratulsikder/neolife/TaskContract.java
@@ -10,7 +10,11 @@ public final class TaskContract {
     public  static  final String SQL_CREATE_ENTRIES  = "CREATE TABLE "+ TaskEntry.TABLE_NAME + " (" +
                                                 TaskEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
                                                 TaskEntry.COLUMN_NAME_TITLE + " TEXT, "+
-                                                TaskEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
+                                                TaskEntry.COLUMN_NAME_SUBTITLE + " TEXT, "+
+                                                TaskEntry.COLUMN_NAME_DESCRIPTION + " TEXT, "+
+                                                TaskEntry.COLUMN_NAME_DUE_DATE + " DATE, "+
+                                                TaskEntry.COLUMN_NAME_CREATED_DATE + " DATE, "+
+                                                TaskEntry.COLUMN_NAME_UPDATED_DATE + " DATE)";
     private TaskContract() {}
 
 
@@ -20,5 +24,9 @@ public final class TaskContract {
         public static final String TABLE_NAME = "tasks";
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+        public static final String COLUMN_NAME_DESCRIPTION = "description";
+        public static final String COLUMN_NAME_DUE_DATE = "due_date";
+        public static final String COLUMN_NAME_CREATED_DATE = "created_date";
+        public static final String COLUMN_NAME_UPDATED_DATE = "updated_date";
     }
 }
